b3e2dce80e14286831fafe2c9b486f466c818b06
ptflo <petrflorian18@seznam.cz>
2023-07-20 01:11:24 +0200

    Init. files update, project theme changed

---
+++ Models/Domain/Trail.cs
@@ -0,0 +1,15 @@
+﻿namespace CZTrails.Models.Domain
+{
+    public class Trail
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public double Length { get; set; }
+        public Guid RegionID { get; set; }
+        public Guid TrailDifficultyID { get; set; }
+
+        //navigation properties
+        public Region Region { get; set; }
+        public TrailDifficulty TrailDifficulty { get; set; } //connecting to the other  domains
+    }
+}
